162bb04b1547df15d6c48a34ac056a11fd8bbd05
Petr Michalec <epcim@apealive.net>
2017-10-31 15:14:32 +0100

    try to fix excapes
    Change-Id: I7c4329dfcb44760de17ba0edcfaacecd809f525f

--- bootstrap.sh
+++ bootstrap.sh
@@ -622,7 +622,7 @@ saltmaster_init() {
 
     # workarond isolated and not fully bootstraped environments
     if [[ $RECLASS_IGNORE_CLASS_NOTFOUND =~ ^(True|true|1|yes)$ ]]; then
-      SALT_MASTER_PILLAR='"salt":{"master":{"pillar":{"reclass":{"ignore_class_notfound": "\'${RECLASS_IGNORE_CLASS_NOTFOUND:-False}\'", "ignore_class_regexp": ["service.*"]}}}},'
+      SALT_MASTER_PILLAR='"salt":{"master":{"pillar":{"reclass":{"ignore_class_notfound": "\\'${RECLASS_IGNORE_CLASS_NOTFOUND:-False}\\'", "ignore_class_regexp": ["service.*"]}}}},'
     fi
     PILLAR='{${SALT_MASTER_PILLAR} "reclass":{"storage":{"data_source":{"engine":"local"}}} }'
 
