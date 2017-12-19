4f043770a6fb3552b3f5a142f75048826009238d
Petr Michalec <epcim@apealive.net>
2017-12-19 15:40:54 +0100

    Additionally start salt services
    Change-Id: I021876c8c3e9d701e332105fb2837535e5941a62

--- bootstrap.sh
+++ bootstrap.sh
@@ -667,6 +667,7 @@ saltmaster_init() {
     # finally re-configure salt master conf, ie: may remove ignore_class_notfound option
     log_info "State: salt.master.service"
     retry ${SALT_STATE_RETRY} $SUDO salt-call ${SALT_OPTS} state.apply salt.master.service || true
+    saltservice_start
 
     log_info "State: salt.master.storage.node"
     set +e
