d2ba9377acd371f1710878ccbadd5f202e1cd975
Petr Michalec <epcim@apealive.net>
2018-03-17 15:08:49 +0100

    Revert "Avoid to install reclass out of install_reclass fn"
    This reverts commit bebd7d5a35434e8d055a70344a89c3c9e6e422a4.

--- bootstrap.sh
+++ bootstrap.sh
@@ -249,8 +249,9 @@ system_config_master() {
     if ! grep '127.0.1.2.*salt' /etc/hosts; then
       echo "127.0.1.2  salt" | $SUDO tee -a /etc/hosts >/dev/null
     fi
-    
-    # DEPRECATED, should be removed or moved to salt master specific function
+
+    which reclass || $SUDO $PKGTOOL install -y reclass
+
     which reclass-salt || {
       test -e /usr/share/reclass/reclass-salt && {
         ln -fs /usr/share/reclass/reclass-salt /usr/bin
