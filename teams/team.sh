f111cf94d029ce8c8b5d1359a719d15205be2b3f
Petr Michalec <epcim@apealive.net>
2018-05-30 17:01:25 +0200

    Update team create script
    Change-Id: Id39108b07ff55a0826be8a9b1df8cbcaa0c58d9e

--- teams/team.sh
+++ teams/team.sh
@@ -5,5 +5,5 @@
 
 awk -F';' '\
   { fname=$1;gsub(/\s+/,_,$1); sname=tolower(substr($1,1,1)substr($1,index(fname," "))); split($4, key ," "); }
-  { print "FNAME=\\\""fname"\\\"", "SNAME=\\\""sname"\\\"", "KEY=\\\""key[1]" "key[2]"\\\"","EMAIL="$2" envsubst < team.template > "tolower($1)".yml" }'\
-  < $1 | xargs -n11 echo
+  { print "FNAME=\""fname"\"", "SNAME=\""sname"\"", "KEY=\""key[1]" "key[2]"\"","EMAIL="$2" envsubst < team.template > "tolower($1)".yml" }'\
+  < $1
